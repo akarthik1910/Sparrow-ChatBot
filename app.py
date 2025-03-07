e6463e8f8eb481b212bdc9fabe3d795f1603877e
Karthik Amrutavakkula <akarthik1910@gmail.com>
2025-03-07 10:48:25 +0530

    Updated app.py and chatbot_responses

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ app = Flask(__name__)
 #with open("chat_bot_responses.json", "r") as file:
     #chatbot_responses = json.load(file)
 # Load responses from Excel
-df = pd.read_excel("chatbot_responses.xlsx")
-chatbot_responses = dict(zip(df["User_Input"].str.lower(), df["Bot_Response"]))
+#df = pd.read_excel("chatbot_responses.xlsx")
+#chatbot_responses = dict(zip(df["User_Input"].str.lower(), df["Bot_Response"]))
+
+def load_responses(file_path='chatbot_responses.xlsx'):
+    df = pd.read_excel(file_path,dtype={'Question':str,'Response':str})
+    df["Question"]=df["Question"].apply(lambda x:str(x).strip().replace(" ",""))
+    return dict(zip(df['Question'].str.lower(), df['Response']))
+
+chatbot_responses=load_responses()
 
 @app.route("/")
 def home():
@@ -16,7 +23,8 @@ def home():
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    user_message = request.json.get("message", "").lower()
+    user_message = request.json.get("message", "").lower().strip()
+    user_message=user_message.replace(" ","")
     # Get response from JSON, fallback to "default" if not found
     response = chatbot_responses.get(user_message, chatbot_responses["default"])
     return jsonify({"response": response})
